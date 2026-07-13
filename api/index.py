c7b955b0c692112a4c7755dcc28460ae232518ac
z0b1 <yu4tmb@gmail.com>
2026-07-14 00:49:37 +0200

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from prisma import Prisma
+import random
+import string
+import os
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+templates = Jinja2Templates(directory=os.path.join(current_dir, "templates"))
+
+db = Prisma()
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+@app.on_event("shutdown")
+async def shutdown():
+    if db.is_connected():
+        await db.disconnect()
+
+def generate_short_code(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/shorten", response_class=HTMLResponse)
+async def create_short_url(request: Request, long_url: str = Form(...)):
+    short_code = generate_short_code()
+    existing = await db.url.find_unique(where={"shortCode": short_code})
+    while existing:
+        short_code = generate_short_code()
+        existing = await db.url.find_unique(where={"shortCode": short_code})
+    await db.url.create(
+        data={
+            "longUrl": long_url,
+            "shortCode": short_code
+        }
+    )
+
+    base_url = str(request.base_url)
+    full_short_url = f"{base_url}{short_code}"
+
+    return templates.TemplateResponse("index.html", {
+        "request":request,
+        "short_url": full_short_url,
+        "long_url": long_url
+    })
+
+@app.get("/{short_code}")
+async def redirect_to_long_url(short_code: str):
+
+    record = await db.url.find_unique(where={"shortCode": short_code})
+
+    if record:
+        return RedirectResponse(record.longUrl, status_code = 307)
+    
+    raise HTTPException(status_code=404, detail="Short URL not found")
